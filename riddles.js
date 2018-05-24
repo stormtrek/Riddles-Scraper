362b7b11fe37de906f0de825a28ac0e63291318b
Andy C <stormtrek@yahoo.com>
2018-05-24 04:07:56 -0400

    Refactored

--- riddles.js
+++ riddles.js
@@ -1,34 +1,25 @@
-let request = require('request');
+let fetch = require('node-fetch');
 let JSSoup = require('jssoup').default;
 
 function getRandomRiddle() {
     let pages = 22;
     let page = Math.floor(Math.random() * pages) + 1;
 
-    return new Promise((resolve, reject) => {
-        request("https://riddles.fyi/page/" + page.toString(), {timeout: 2000}, (err, res, body) => {
-            try {
-                if (err) {
-                    resolve({"success": false, "message": err.code})
-                }
+    return fetch("http://www.heavens-above.com", {timeout: 0})
+        .then(res => res.text())
+        .then(body => {
+          let soup = new JSSoup(body);
+  
+          let posts = soup.findAll("article");
+          let post = posts[Math.floor(Math.random() * posts.length)]
+          
+          soup = new JSSoup(post);
+  
+          let riddle = soup.find("h2", "entry-title").text;
+          let answer = soup.find("div", "su-spoiler-content").text;
 
-                let soup = new JSSoup(body);
-        
-                let posts = soup.findAll("article");
-                let post = posts[Math.floor(Math.random() * posts.length)]
-                
-                soup = new JSSoup(post);
-        
-                let riddle = soup.find("h2", "entry-title").text;
-                let answer = soup.find("div", "su-spoiler-content").text;
-
-                resolve({"success": true, "riddle": riddle, "answer": answer});
-            } 
-            catch (err) {
-                resolve({"success": false, "message": "something went wrong"});
-            }
+          return { riddle, answer }
         })
-    })
 }
 
 exports.getRandomRiddle = getRandomRiddle;
--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,12 @@ let riddles = require("../riddles.js");
 
 let appRouter = function(app) {
     app.get("/riddle", (req, res) => {
-        let getRiddle = riddles.getRandomRiddle();
-        getRiddle.then((data) => {
-            if (data["success"]) {
-                return res.send({"status": "ok", "riddle": data["riddle"], "answer": data["answer"]});
-            } else {
-                return res.send({"status": "error", "message": data["message"]});
-            }
+        riddles.getRandomRiddle()
+        .then((result) => {
+            return res.send(result);
+        })
+        .catch((err) => {
+            return res.send({error: "something went wrong"});
         })
     });
 
